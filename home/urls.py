e7733e66091c0d5f238eefeca4f76019b9d33c95
Reekomer <jeremy.degail@gmail.com>
2017-09-18 17:44:55 +0200

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
 from .api import StoyoViewSet
 from .api import PublisherViewSet
+from .api import TemporaryViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'stoyo', StoyoViewSet)
 router.register(r'publisher', PublisherViewSet)
+router.register(r'temporary', TemporaryViewSet)
 
 urlpatterns = router.urls
