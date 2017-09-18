0aac9403c87c37a077733fc57066d91872e7936c
Reekomer <jeremy.degail@gmail.com>
2017-09-18 17:41:01 +0200

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Stoyo
 from .models import Publisher
+from .models import Temporary
 
 # Register your models here.
 
 admin.site.register(Stoyo)
 admin.site.register(Publisher)
+admin.site.register(Temporary)
