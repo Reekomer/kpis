f71b2596cc3228e3b0ccb200cb32ce6f33540895
Reekomer <jeremy.degail@gmail.com>
2017-09-18 17:42:43 +0200

    Update api.py

--- home/api.py
+++ home/api.py
@@ -2,9 +2,11 @@ from rest_framework.viewsets import ModelViewSet
 
 from .serializers import StoyoSerializer
 from .serializers import PublisherSerializer
+from .serializers import TemporarySerializer
 
 from .models import Stoyo
 from .models import Publisher
+from .models import Temporary
 
 class StoyoViewSet(ModelViewSet):
     queryset = Stoyo.objects.all()
@@ -13,3 +15,7 @@ class StoyoViewSet(ModelViewSet):
 class PublisherViewSet(ModelViewSet):
     queryset = Publisher.objects.all()
     serializer_class = PublisherSerializer
+
+class TemporaryViewSet(ModelViewSet):
+    queryset = Temporary.objects.all()
+    serializer_class = TemporarySerializer
